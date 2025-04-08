09881d9eb8333ea504b8f4baafa2d14a8b15b54d
Ádám Székely <stereotribe@hotmail.com>
2025-04-08 17:03:37 +0200

    fix(hash): added return type to instance provider factory

--- src/hash/hash.module.ts
+++ src/hash/hash.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { HashService } from './hash.service';
 import { HASH_INSTANCE } from './hash.constants';
 
-import xxhash from 'xxhash-wasm';
+import xxhash, { XXHashAPI } from 'xxhash-wasm';
 
 @Module({
     exports: [HashService],
     providers: [
         {
             provide: HASH_INSTANCE,
-            useFactory: () => {
+            useFactory: (): Promise<XXHashAPI> => {
                 // Extremely fast non-cryptographic algorithm with high velocity on small
                 // data, loaded asynchronously due to WebAssembly initialization
 
